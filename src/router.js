a10231bb7363170d42d0dc865caa0ea59a8ab14c
chendidi <757960181@qq.com>
2017-10-18 18:34:10 +0800

    动态加载路由

--- src/router.js
+++ src/router.js
@@ -1,36 +1,36 @@
 import React from 'react';
 import { Router, Switch, Route } from 'dva/router';
 import dynamic from 'dva/dynamic';
-import LoginPage from './routes/login/Login';
-import IndexPage from './routes/IndexPage';
-import App from './routes/App';
+// import LoginPage from './routes/login/Login';
+// import IndexPage from './routes/IndexPage';
+// import App from './routes/App';
 
 function RouterConfig({ history, app }) {
-  // const IndexPage = dynamic({
-  //   app,
-  //   component: () => import('./routes/IndexPage'),
-  // });
-  //
-  // const App = dynamic({
-  //   app,
-  //   component: () => import('./routes/App'),
-  // });
-  //
-  // const LoginPage = dynamic({
-  //   app,
-  //   // models: () => [
-  //   //   import('./models/users'),
-  //   // ],
-  //   component: () => import('./routes/login/Login'),
-  // });
+  const IndexPage = dynamic({
+    app,
+    component: () => import('./routes/IndexPage'),
+  });
+
+  const App = dynamic({
+    app,
+    component: () => import('./routes/App'),
+  });
+
+  const LoginPage = dynamic({
+    app,
+    // models: () => [
+    //   import('./models/users'),
+    // ],
+    component: () => import('./routes/login/Login'),
+  });
 
   return (
     <Router history={history}>
-      {/* <switch>*/}
-      <Route path="/" component={LoginPage} />
-      <Route path="/index" component={IndexPage} />
-      <Route path="/app" component={App} />
-      {/* </switch>*/}
+      <switch>
+        <Route exact path="/" component={LoginPage} />
+        <Route exact path="/index" component={IndexPage} />
+        <Route exact path="/app" component={App} />
+      </switch>
     </Router>
   );
 }
